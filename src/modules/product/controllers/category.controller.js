104ab3a8e6d826655de0032e09b3ca974bdc1e19
Aliaa18 <aliaasultan75@gmail.com>
2025-02-16 18:02:56 +0200

    third commit

--- src/modules/product/controllers/category.controller.js
+++ src/modules/product/controllers/category.controller.js
@@ -17,8 +17,8 @@ export const getCategory = catchAsyncError(async (req, res) => {
 
 export const getCategories = catchAsyncError(async (req, res) => {
 	const apiFeatures = new ApiFeatures(
-		categoryModel.find() .populate({
-			path: 'subcategory',
+		categoryModel.find().populate({
+			path: 'subcategories',
 			populate: {
 			  path: 'products', // Populate products inside each subcategory
 			  model: 'product'
