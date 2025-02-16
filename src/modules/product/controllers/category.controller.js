c970dad440c8306d87fd7742d38c55ff4b8f6296
Aliaa18 <aliaasultan75@gmail.com>
2025-02-16 17:58:35 +0200

    third commit

--- src/modules/product/controllers/category.controller.js
+++ src/modules/product/controllers/category.controller.js
@@ -4,13 +4,27 @@ import categoryModel from '../models/category.model.js'
 
 export const getCategory = catchAsyncError(async (req, res) => {
 	const { categorySlug } = req.params
-	const category = await categoryModel.findOne({ slug: categorySlug })
+	const category = await categoryModel.findOne({ slug: categorySlug }) .populate({
+        path: 'subcategories',
+        populate: {
+          path: 'products', // Populate products inside each subcategory
+          model: 'product'
+        }
+      })
+      .populate('products');
 	res.json({ category })
 })
 
 export const getCategories = catchAsyncError(async (req, res) => {
 	const apiFeatures = new ApiFeatures(
-		categoryModel.find(),
+		categoryModel.find() .populate({
+			path: 'subcategories',
+			populate: {
+			  path: 'products', // Populate products inside each subcategory
+			  model: 'product'
+			}
+		  })
+		  .populate('products'),
 		req.query
 	).paginate(10)
 	const categories = await apiFeatures.query
