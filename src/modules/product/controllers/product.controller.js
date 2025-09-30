fd3cfd1a8a1a74e59759060c3e45d7f429b33a1f
Aliaa18 <aliaasultan75@gmail.com>
2025-10-01 01:32:00 +0300

    third commit

--- src/modules/product/controllers/product.controller.js
+++ src/modules/product/controllers/product.controller.js
@@ -35,10 +35,25 @@ export const addProductWithImages = catchAsyncError(async (req, res, next) => {
 	  }
 	const categoryId = subcategory.category_id;
 	// console.log(categoryId);
-// 	  let status = "approved";
-//   if (user.role === "SEMIADMIN") {
-//     status = "pending";
-//   }
+		if (user.role ==="SEMIADMIN"){
+			const msg = {
+		to: process.env.EMAIL, // 📥 Your internal email (sales, admin, etc.)
+		from: process.env.EMAIL , // 📤 Sender (same if you're using one verified domain/email)
+		subject: 'New User Signup Notification',
+		text: `A new .`,
+		html: `
+			<h2>New Signup</h2>
+			
+		`,
+	
+	  };
+	 try {
+    let info = await transporter.sendMail(msg);
+    console.log("Message sent: %s", info.messageId);
+  } catch (err) {
+    console.error("Error sending email:", err);
+  }
+		}
 	const productData = { ...req.body, category_id: categoryId  }
         
 	const product = await productModel.create(productData)
@@ -64,25 +79,7 @@ export const addProductWithImages = catchAsyncError(async (req, res, next) => {
 				}
 			})
 		)
-// 		if (user.role ==="SEMIADMIN"){
-// 			const msg = {
-// 		to: process.env.EMAIL, // 📥 Your internal email (sales, admin, etc.)
-// 		from: process.env.EMAIL , // 📤 Sender (same if you're using one verified domain/email)
-// 		subject: 'New User Signup Notification',
-// 		text: `A new .`,
-// 		html: `
-// 			<h2>New Signup</h2>
-			
-// 		`,
-	
-// 	  };
-// 	 try {
-//     let info = await transporter.sendMail(msg);
-//     console.log("Message sent: %s", info.messageId);
-//   } catch (err) {
-//     console.error("Error sending email:", err);
-//   }
-// 		}
+
 
 	res.status(201).json({
 		message: `Added product with ${req.files.images?.length || 0} images`, user
