652aeb3012ae207d63acfd79fbb03420a8dbf90d
Aliaa18 <aliaasultan75@gmail.com>
2025-10-02 17:04:58 +0300

    em commit

--- src/utils/email.js
+++ src/utils/email.js
@@ -2,10 +2,10 @@ import nodemailer from 'nodemailer'
 import dotenv from 'dotenv'
 dotenv.config()
 const transporter = nodemailer.createTransport({
-     service:'gmail',
+     service:'outlook',
 	auth: {
-		user: "ms8159974@gmail.com",
-		pass: "mlam xixd dgvg vjdk",
+		user: "alaa.mohamed@mci-egypt.com",
+		pass: "jlzuohiawocevnuu",
 	},
 })
 
