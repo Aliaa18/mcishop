acdfb02781d6443175bcf8aad36ca4684cb3ee6a
Aliaa18 <aliaasultan75@gmail.com>
2025-10-07 19:58:35 +0300

    third commit

--- src/modules/product/controllers/productApprovalController.js
+++ src/modules/product/controllers/productApprovalController.js
@@ -1,4 +1,5 @@
 import imageModel from "../../image/models/image.model.js";
+import imageOnProductModel from "../models/imageOnProduct.js";
 import pendingProductModel from "../models/pendingProduct.model.js";
 import productModel from "../models/product.model.js";
 import product from "../models/product.model.js";
