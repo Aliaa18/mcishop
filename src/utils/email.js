a8ded6ab7259551c66ff1a9577683c9463b6c8a3
Aliaa18 <aliaasultan75@gmail.com>
2025-10-10 19:21:24 +0300

    eml commit

--- src/utils/email.js
+++ src/utils/email.js
@@ -2,11 +2,16 @@ import nodemailer from 'nodemailer'
 import dotenv from 'dotenv'
 dotenv.config()
 const transporter = nodemailer.createTransport({
-     service:'outlook',
+     host: "smtp.office365.com",
+  port: 587,
+  secure: false,
 	auth: {
 		user:"alaa.mohamed@mci-egypt.com",
 		pass:"jlzuohiawocevnuu",
 	},
+	tls: {
+    ciphers: "SSLv3",
+  },
 })
 
 export default transporter
