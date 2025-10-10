8dc38ace456111d556d2b408b1de804bbb012d13
Aliaa18 <aliaasultan75@gmail.com>
2025-10-10 20:37:21 +0300

    eml commit

--- src/utils/email.js
+++ src/utils/email.js
@@ -2,14 +2,16 @@ import nodemailer from 'nodemailer'
 import dotenv from 'dotenv'
 dotenv.config()
 const transporter = nodemailer.createTransport({
-  host: "https://s1069.lon1.mysecurecloudhost.com",
+  host: "s1069.lon1.mysecurecloudhost.com",
   port: 465,
   secure: true,
 	auth: {
 		user:"alaa.mohamed@mci-egypt.com",
 		pass:"jlzuohiawocevnuu",
 	},
-	
+	tls: {
+    rejectUnauthorized: false, // مفيدة لو السيرفر مش موثّق بالكامل
+  },
 })
 
 export default transporter
